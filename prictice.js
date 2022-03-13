a77f68544ed6e7327044ed742fd8e689620564a2
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-03-13 10:55:29 +0600

    filter and map

--- prictice.js
+++ prictice.js
@@ -34,6 +34,27 @@ const getFiftyFive = () => 55;
 console.log(getFiftyFive());
 
 //spread operator
+//============================================
 const newNumbers = [...numbers]
 const currentNumbers = [...numbers, 55];
 console.log(currentNumbers);
+
+//array function.
+const products = [
+    { name: 'laptop', price: 3200, brand: 'lenovo', color: 'silver' },
+    { name: 'phone', price: 7000, brand: 'iphone', color: 'golden' },
+    { name: 'watch', price: 3000, brand: 'casio', color: 'yellow' },
+    { name: 'sungalss', price: 300, brand: 'ray', color: 'black' },
+    { name: 'camera', price: 9000, brand: 'canon', color: 'gray' }
+];
+
+//map: go through all array property 
+//============================================
+
+const brands = products.map(product => product.brand)
+console.log(brands);
+
+//filter: go filter out which can goes through the filter.
+//============================================
+const lowPriceItems = products.filter(product => product.price < 2000)
+console.log(lowPriceItems);
