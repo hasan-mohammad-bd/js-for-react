0e01213f2e5ff454ed664abd3eb5faa7715e0319
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-03-13 10:18:30 +0600

    templete string

--- prictice.js
+++ prictice.js
@@ -1,3 +1,4 @@
+//fundamentals
 //3 ways to access object property.
 
 const student = {
@@ -10,3 +11,17 @@ const theAge = "age";
 console.log(student.age);
 console.log(student["age"]);
 console.log(student[theAge]);
+
+//es6
+const numbers = [89, 35, 98, 12];
+const student2 = {
+    name: 'Salib Khan',
+    age: 32,
+    movies: ['king khan', 'Dhakar Mastan']
+};
+
+//template string.
+const about =  `
+his name is ${student2.name} his age is ${student2.age}, his movie names are ${student2.movies}. the number the got is ${numbers[0]}.
+`
+console.log(about);
